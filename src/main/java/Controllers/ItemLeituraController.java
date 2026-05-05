b7c8df7ed0757440d37867dfc338ebd515cca482
Misael Silva Oliveira <misaelsilvaoliveira1@gmail.com>
2026-05-05 16:29:43 -0300

    commit inicial

---
+++ src/main/java/Controllers/ItemLeituraController.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controllers;
+
+/**
+ *
+ * @author Usuário
+ */
+public class ItemLeituraController {
+    public void SalvarItem(){
+       /*Método*/
+    }
+}
---
+++ src/main/java/model/ItemLeitura.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Usuário
+ */
+public class ItemLeitura {
+    private String autor;
+    private String titulo;
+    private int numeroPaginas;
+    private int anoPublicacao;
+    private String genero;
+    private int edicao;
+    private int avaliacao;
+}
