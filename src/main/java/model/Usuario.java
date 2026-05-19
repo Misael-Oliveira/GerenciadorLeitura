6b8ec68d25ce68f76a10b20e7ddc62b0b8cb7709
Misael Silva Oliveira <82273415+Misael-Oliveira@users.noreply.github.com>
2026-05-19 15:25:45 -0300

    adicionando comentários

--- src/main/java/model/Usuario.java
+++ src/main/java/model/Usuario.java
@@ -22,37 +22,22 @@ public class Usuario {
         return nome;
     }
 
-    /**
-     * @param nome the nome to set
-     */
     public void setNome(String nome) {
         this.nome = nome;
     }
 
-    /**
-     * @return the email
-     */
     public String getEmail() {
         return email;
     }
 
-    /**
-     * @param email the email to set
-     */
     public void setEmail(String email) {
         this.email = email;
     }
 
-    /**
-     * @return the idade
-     */
     public int getIdade() {
         return idade;
     }
 
-    /**
-     * @param idade the idade to set
-     */
     public void setIdade(int idade) {
         this.idade = idade;
     }
